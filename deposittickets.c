d5825d5b073dd38650595828b7d2dcad26f06add
Gabriel Henrique Rudey <ucraaa@gmail.com>
2016-11-10 22:20:16 -0200

    HTLE

--- deposittickets.c
+++ deposittickets.c
@@ -1,6 +1,5 @@
 #include "types.h"
 #include "param.h"
-#include "stat.h"
 #include "syscall.h"
 #include "user.h"
 
@@ -8,8 +7,8 @@ int main(int argc, char *argv[])
 {
   if (argc > 1) deposittickets(atoi(argv[1]));
   else {
-    printf(1, "Atribui um numero de tickets para o proximo processo a ser criado.\n");
-    printf(1, "Uso: %s numero_tickets\n", argv[0]);
+    printf(1, "Assign the tickets to a next process\n");
+    printf(1, "Use: %s tickets\n", argv[0]);
   }
 
   exit();
